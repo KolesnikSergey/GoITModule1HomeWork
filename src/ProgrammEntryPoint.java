b67409293282a6ff10f9db47b02f8253b33dbd3c
Sergey Kolesnik <s.kolesnik@bpcbt.com>
2015-12-12 17:29:06 +0200

    add src directory

---
+++ src/ProgrammEntryPoint.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kolesnik_s on 12.12.15.
+ */
+public class ProgrammEntryPoint {
+    public static void main (String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
