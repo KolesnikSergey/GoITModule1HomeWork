c58b669752295f0242457389684244ec2aa176f7
Sergey Kolesnik <s.kolesnik@bpcbt.com>
2015-12-13 15:13:11 +0200

    Git testing

--- src/ProgrammEntryPoint.java
+++ src/ProgrammEntryPoint.java
@@ -3,6 +3,6 @@
  */
 public class ProgrammEntryPoint {
     public static void main (String[] args) {
-        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+        System.out.println("Git from Idea test.");
     }
 }
